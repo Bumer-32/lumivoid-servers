c8d31a96537711aecb08901fa18c5764a1d8caa1
Artem <krutog727@gmail.com>
2024-05-11 12:15:36 +0300

    Initial commit

---
+++ src/main/kotlin/ua/pp/lumivoid/lumivoidservers/LumivoidServersApplication.kt
@@ -0,0 +1,11 @@
+package ua.pp.lumivoid.lumivoidservers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LumivoidServersApplication
+
+fun main(args: Array<String>) {
+	runApplication<LumivoidServersApplication>(*args)
+}
---
+++ src/test/kotlin/ua/pp/lumivoid/lumivoidservers/LumivoidServersApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.pp.lumivoid.lumivoidservers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LumivoidServersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
